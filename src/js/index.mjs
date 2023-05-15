817ca040694257ef16755e1f5e82a2572e859d7b
jkjode <104823459+jkjode@users.noreply.github.com>
2023-05-15 22:39:59 +0200

    get post functions

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,6 +1,8 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
-import * as post from "./api/posts/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
+import * as templates from "./templates/index.mjs";
+// import { renderPostTemplates } from "./templates/index.mjs";
 
 const path = location.pathname;
 
@@ -10,21 +12,11 @@ if (path === "/profile/login/index.html") {
   setRegisterFormListener();
 }
 
-// createPost({
-//   title: "My first post",
-//   body: "This is my first post",
-// });
+async function testTemplate() {
+  const posts = await postMethods.getPosts();
+  // const post = posts.pop(); 
+  const container = document.querySelector("#post");
+  templates.renderPostTemplates(posts, container);
+}
 
-// updatePost({
-//   id: 5906,
-//   title: "My first post UPDATED",
-//   body: "This is my first UPDATED post",
-// });
-
-// removePost(5906);
-
-// post.createPost()
-// post.updatePost()
-// post.removePost()
-// post.getPost()
-post.getPosts().then(console.log);
+testTemplate();
